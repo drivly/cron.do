72e25e83b32488adfbc612108814b0dc5f562b2d
Nathan Clevenger <nateclev@gmail.com>
2022-12-01 12:13:46 -0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,18 @@
 export const api = {
   icon: '🚀',
-  name: 'templates.do',
-  description: 'Cloudflare Worker Template',
-  url: 'https://templates.do/api',
-  type: 'https://apis.do/templates',
+  name: 'cron.do',
+  description: 'CRON Scheduler API',
+  url: 'https://cron.do/api',
+  type: 'https://apis.do/tools',
   endpoints: {
-    listCategories: 'https://templates.do/api',
-    getCategory: 'https://templates.do/:type',
+    listCategories: 'https://cron.do/api',
+    getCategory: 'https://cron.do/:type',
   },
-  site: 'https://templates.do',
-  login: 'https://templates.do/login',
-  signup: 'https://templates.do/signup',
-  subscribe: 'https://templates.do/subscribe',
-  repo: 'https://github.com/drivly/templates.do',
+  site: 'https://cron.do',
+  login: 'https://cron.do/login',
+  signup: 'https://cron.do/signup',
+  subscribe: 'https://cron.do/subscribe',
+  repo: 'https://github.com/drivly/cron.do',
 }
 
 export const gettingStarted = [
@@ -21,7 +21,7 @@ export const gettingStarted = [
 ]
 
 export const examples = {
-  listItems: 'https://templates.do/worker',
+  listItems: 'https://cron.do/worker',
 }
 
 export default {
